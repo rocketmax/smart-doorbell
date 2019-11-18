5d1f775090359ec0fa43c4e1a19651be4a836c8b
rocketmax <maxt@umich.edu>
2019-11-17 22:03:36 -0500

    manual merge

---
+++ networking/rx/receive.cpp
@@ -0,0 +1,103 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+using namespace std;
+using namespace cv;
+
+#define PORT 8080
+
+void printV(vector<uchar> v){
+
+  vector<uchar>::iterator p;
+  for(p = v.begin(); p < v.end(); p++)
+    cout << int(*p) << '\t';
+  cout << endl;
+}
+
+Mat vecs2mat(vector<vector<uchar>> v){
+
+  Mat image(0, v[0].size(), CV_8UC1);
+
+  for(int i = 0; i < v.size(); ++i){
+    Mat sample(1, v[0].size(), CV_8UC1, v[i].data());
+    image.push_back(sample);
+  }
+  return image;
+}
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char sz_buf[10] = {0};
+    char buffer[1920] = {0};
+    //Mat image;
+    //vector<uchar> iv;
+    //iv.reserve(1024);
+    //vector<uchar> *ptr = &iv;
+
+    uchar * a;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    //if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, "10.4.5.21", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    //valread = read( sock , ptr, 1024);
+    valread = read( sock, sz_buf, 1920);
+    char * height = strtok (sz_buf,",");
+    char * width = strtok (NULL, ",");
+    int h = atoi(height);
+    int w = atoi(width);
+    //valread = read(sock, buffer, 1920);
+    //int w = atoi(buffer);
+    cout << h << '\t' << w << endl;
+    vector<vector<uchar>> im;
+
+    for(int i = 0; i < h; i++){
+      valread = read(sock, buffer, w);
+      vector<uchar> temp(buffer, buffer+w);
+      im.push_back(temp);
+    }
+
+    a = reinterpret_cast<uchar*>(buffer);
+
+    Mat image = vecs2mat(im);
+    //printf("%s\n",buffer );
+    //for(int i = 0; i < 81000; i++)
+    //  cout << int(a[i]) << '\t';
+    imshow("image", image);
+    //printV(iv);
+    waitKey(0);
+    return 0;
+}
+
---
+++ networking/tx/sendVideo.cpp
@@ -0,0 +1,148 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <vector>
+
+#define PORT 8080
+
+using namespace std;
+using namespace cv;
+
+vector<uchar> mat2vec(Mat mat){
+
+  std::vector<uchar> array;
+  if (mat.isContinuous())
+    array.assign(mat.data, mat.data + mat.total());
+  else
+    for (int i = 0; i < mat.rows; ++i)
+      array.insert(array.end(), mat.ptr<uchar>(i), mat.ptr<uchar>(i)+mat.cols);
+  return array;
+}
+
+vector<vector<uchar>> mat2vecs(Mat mat){
+
+  Mat row;
+  vector<uchar> temp;
+  vector<vector<uchar>> array;
+
+  for (int i = 0; i < mat.rows; ++i){
+    row = mat.row(i);
+    temp.clear();
+    temp.insert(temp.end(), row.ptr<uchar>(0), row.ptr<uchar>(0)+row.cols);
+    array.push_back(temp);
+  }
+
+  cout << array.size() << '\t' << array[0].size() << endl;
+  return array;
+}
+
+void printV(vector<uchar> v){
+
+  vector<uchar>::iterator p;
+  for(p = v.begin(); p < v.end(); p++)
+    cout << int(*p) << '\t';
+  cout << endl;
+}
+
+void printVs(vector<vector<uchar>> v){
+
+  vector<vector<uchar>>::iterator ptr;
+  for(ptr = v.begin(); ptr < v.end(); ++ptr)
+    printV(*ptr);
+}
+
+Mat vecs2mat(vector<vector<uchar>> v){
+
+  Mat image(0, v[0].size(), CV_8UC1);
+
+  for(int i = 0; i < v.size(); ++i){
+    Mat sample(1, v[0].size(), CV_8UC1, v[i].data());
+    image.push_back(sample);
+  }
+  return image;
+}
+
+int main(int argc, char *argv[]){
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+
+    Mat bwi, image = imread("/home/ubuntafoo/Downloads/dog.jpg", CV_LOAD_IMAGE_COLOR);
+    cvtColor(image, bwi, COLOR_BGR2GRAY);
+    bwi.convertTo(bwi, CV_8UC1);
+    vector<vector<uchar>> iv = mat2vecs(bwi);
+    //printVs(test);
+    //namedWindow( "image", WINDOW_AUTOSIZE );
+    //Mat testim = vecs2mat(test);
+    //imshow("image", testim);
+    //waitKey(0);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+
+    char sz[10];
+    sprintf( sz, "%d,%d", bwi.rows, bwi.cols);
+    cout << sz << endl;
+    //sprintf( w, "%d", bwi.cols);
+    send(new_socket, sz, strlen(sz), 0);
+
+    for(int i = 0; i < bwi.rows; i ++){
+      send(new_socket, iv[i].data(), bwi.cols, 0);
+      usleep(500);
+    }
+
+    //send(new_socket, w, strlen(w), 0);
+    //send(new_socket , hello , strlen(hello) , 0 );
+    //send(new_socket, reinterpret_cast<char*>(a), image.rows*image.cols, 0);
+    //printf("Hello message sent\n");
+    return 0;
+}
+
