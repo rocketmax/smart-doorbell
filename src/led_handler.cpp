39d61f83dbad5871f13d99c9597e8a3372099b30
rocketmax <57817987+rocketmax@users.noreply.github.com>
2019-12-09 20:44:29 -0500

    Update led_handler.cpp

--- src/led_handler.cpp
+++ src/led_handler.cpp
@@ -6,12 +6,12 @@
 void led_handler(){
 
   for(;;){
-    if (light <= 40 && !led){
+    if (light <= 40 && !led){ //threshold to turn LED on
       digitalWrite(0,1);
       std::cout << "LED ON" << std::endl;
       led = 1;
     }
-    else if (light >= 60 && led){
+    else if (light >= 60 && led){ //threshold to turn LED off
       digitalWrite(0, 0);
       std::cout << "LED OFF" << std::endl;
       led = 0;
